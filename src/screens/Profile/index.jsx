d2d71566f9e2ebbbba60acd21da460c93238227a
Enrico Lona <enrico-lona@aluno.senai.br>
2024-03-24 23:20:00 -0300

    feat: estruturação e estilização da página profile

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -2,11 +2,61 @@ import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
 
+import planetaRepositorio from "../../models/PlanetaRepositorio"
 
 export default function Profile({route}) {
+
+  const navigation = useNavigation();
+  const {data} = route.params;
+
+  const editUser = () => {
+    navigation.navigate("Cadastro", {planeta: data, edit: true});
+  };
+
+  const deleteUser = () => {
+    planetaRepositorio.remove(data.id);
+    // navigation.navigate("Planetas");
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <Text style={styles.textPrincipal}>Profile</Text>
+
+      {data ?  (
+        <Text>Detalhes do Planeta</Text>
+      ) : (
+        <Text>Selecione um planeta para exibir seus detalhes</Text>
+      )}
+        
+        <View style={styles.planeta}>
+        <View style={styles.planetaDetail}>
+          <Text style={styles.text}>{data.planet}</Text>
+          <Text style={styles.text}>{data.date}</Text>
+          <Text style={styles.text}>{data.color1}</Text>
+          <Text style={styles.text}>{data.color2}</Text>
+          <Text style={styles.text}>{data.populacao}</Text>
+          <Text style={styles.text}>{data.recursos}</Text>
+          <Text style={styles.text}>{data.assentamentos}</Text>
+          <Text style={styles.text}>{data.galaxia}</Text>
+          <Text style={styles.text}>{data.sistema}</Text>
+          <Text style={styles.text}>{data.coordenadas}</Text>
+          <Text style={styles.text}>{data.frequencia}</Text>
+          <Text style={styles.text}>{data.codificacao}</Text>
+          <Text style={styles.text}>{data.name}</Text>
+          <Text style={styles.text}>{data.cargo}</Text>
+        </View>
+
+        <View style={styles.planetaActions}>
+          <TouchableOpacity style={styles.editButton} onPress={editUser}>
+            <Text>Editar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteButton} onPress={deleteUser}>
+            <Text>Excluir</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -6,6 +6,55 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  }, textPrincipal: {
+    fontSize: 30,
+    textAlign: "center",
+    margin: 0,
+    color: "#fff",
+    marginTop: 100,
+  }, button: {
+    backgroundColor: "#0000FF",
+    borderRadius: 10,
+    margin: 15,
+    width: 72,
+    height: 34,
+    alignItems: "center",
+    padding: 5,
+  },
+  planeta: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#C9D4FF",
+    borderRadius: 5,
+  },
+  planetaDetail: {
+    marginBottom: 10,
+  },
+  planetaActions: {
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "center",
+  },
+  editButton: {
+    backgroundColor: "#0000FF",
+    borderRadius: 10,
+    margin: 15,
+    width: 72,
+    height: 34,
+    alignItems: "center",
+    padding: 5,
+  },
+  deleteButton: {
+    backgroundColor: "#ff0000",
+    borderRadius: 10,
+    margin: 15,
+    width: 72,
+    height: 34,
+    alignItems: "center",
+    padding: 5,
+  },
+  text: {
+    fontSize: 16,
   },
 });
 
